1e3ed96684b458cf5e8727948deea0324ad008d1
Jafrin <khan.jafrin.9346@gmail.com>
2023-05-16 20:03:07 +0530

    Update app.js

--- app.js
+++ app.js
@@ -6,9 +6,6 @@ const ejs = require("ejs");
 const mongoose = require("mongoose")
 const _=require("lodash");
 
-
-
-
 const homeStartingContent = "This is your Journey through the years of glory,fear,motivation,success,failures! Make a new addition to your life experiences and brush up the older ones! you have a beautiful life, cherish it and don't let it down by some symbolic represenation of harsh truth.This is your life and you will decide what course you will take for the better or the worse but the decision is yours.Go on and make a new note of your wonderful life with all the emotions you have got and give a kickstart to a new bright day with all the luck and devotion!"
 const aboutContent = "Soul stories is a Daily Journal to register the events that we want to remember till the end of time.This is made as an initiative to improve the lifestyle since talking to your own thoughts is something that boosts up our confidence.After spending a long day when we pen it down then we realise how productice the day was and it provides us ways to improve our lifestyle.It unravels the loopholes that exist while we work and allows us to introspect our every action for the better good of our next day! So buckle up and pen down your thoughts and be ready for the improvement like never before.";
 const contactContent = "Risque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
@@ -23,16 +20,12 @@ app.use(express.static("public"));
 
 mongoose.connect("mongodb+srv://admin-Jafrin:Test123@cluster0.vacef.mongodb.net/blogDB", {useNewUrlParser: true});
 
-
 const postSchema = {
   title : String,
   content : String
 };
 
 const Post = mongoose.model("Post",postSchema);
-
-
-
 
 app.get("/",function(req,res){
   // my changes
@@ -46,11 +39,7 @@ app.get("/",function(req,res){
 
        });
        });
-  
-
-});
-
-
+ });
 
 app.get("/buySubscription",function(req,res){
   res.render("buySubscription",{buySubscription:buySubscription});
@@ -61,7 +50,6 @@ app.get("/contact",function(req,res){
   res.render("contact",{contactContent:contactContent});
 });
 
-
 app.get("/about",function(req,res){
   res.render("about",{aboutContent:aboutContent});
 });
@@ -69,8 +57,6 @@ app.get("/about",function(req,res){
 app.get("/compose",function(req,res){
   res.render("compose");
 });
-
-
 
 // res.body comes from body parser module that we downloaded and postTitle is the name of the input
 app.post("/compose",function(req,res){
@@ -85,13 +71,8 @@ app.post("/compose",function(req,res){
 
       res.redirect("/");
 
-    }
-
-  });
-
+    }});
 });
-
-
 
 app.get("/posts/:postId",function(req,res){
 
@@ -104,14 +85,9 @@ app.get("/posts/:postId",function(req,res){
        title: post.title,
 
        content: post.content
-
-     });
-
-   });
-
-
-
-      });
+         });
+});
+});
 
 var port = process.env.PORT || 3000;
 app.listen(port, "0.0.0.0", function() {
